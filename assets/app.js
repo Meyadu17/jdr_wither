b5c3173c05a5c4376f70256b04732156377f97bc
Meya <angie-island@hotmail.fr>
2023-10-26 12:14:07 +0200

    controle des champs obligatoirs

--- assets/app.js
+++ assets/app.js
@@ -26,22 +26,22 @@ document.addEventListener('DOMContentLoaded', function () {
 //#endregion ------------------- Accordion -------------------
 
 //#region ------------------- Contrôle form -------------------
-(() => {
-    'use strict'
-
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    const forms = document.querySelectorAll('.needs-validation')
-
-    // Loop over them and prevent submission
-    Array.from(forms).forEach(form => {
-        form.addEventListener('submit', event => {
-            if (!form.checkValidity()) {
-                event.preventDefault()
-                event.stopPropagation()
-            }
-
-            form.classList.add('was-validated')
-        }, false)
-    })
-})()
+// (() => {
+//     'use strict'
+//
+//     // Fetch all the forms we want to apply custom Bootstrap validation styles to
+//     const forms = document.querySelectorAll('.needs-validation')
+//
+//     // Loop over them and prevent submission
+//     Array.from(forms).forEach(form => {
+//         form.addEventListener('submit', event => {
+//             if (!form.checkValidity()) {
+//                 event.preventDefault()
+//                 event.stopPropagation()
+//             }
+//
+//             form.classList.add('was-validated')
+//         }, false)
+//     })
+// })()
 //#endregion ------------------- Contrôle form -------------------
--- src/Form/ArmeType.php
+++ src/Form/ArmeType.php
@@ -12,18 +12,37 @@ use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ArmeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('typeArme', EntityType::class, [
+                'class' => TypeArme::class,
+                'choice_label' => 'libelle',
+                'label' => "Type d'arme*",
+                'required' => true,
+                'placeholder' => 'Choisir le type de l\'arme',
+                'attr' => [
+                    'class' => 'form-select required-field',
+                ],
+            ])
+            ->add('taille', EntityType::class, [
+                'class' => Taille::class,
+                'choice_label' => 'libelle',
+                'label' => "Taille de l'arme*",
+                'required' => true,
+                'placeholder' => 'Choisir la taille', // Message par défaut
+                'attr' => [
+                    'class' => 'form-select required-field',
+                ],
+            ])
             ->add('nom', TextType::class, [
                 'label' => "Nom*",
 
                 'attr' => [
-                    'class' => 'form-control',
+                    'class' => 'form-control required-field',
                     'placeholder' => 'Arc court',
                     'required' => true,
                 ],
@@ -36,31 +55,30 @@ class ArmeType extends AbstractType
                 ],
             ])
             ->add('mains', IntegerType::class, [
-                'label' => "Mains* (0 si null, 1, ou 2)",
+                'label' => "Mains (1, ou 2)",
                 'attr' => [
                     'class' => 'form-control',
                     'placeholder' => 'Saisissez un nombre'
                 ],
             ])
             ->add('portee', IntegerType::class, [
-                'label' => "Portée* (en mettre, 0 si null)",
+                'label' => "Portée (en mettre)",
                 'attr' => [
                     'class' => 'form-control',
                     'placeholder' => 'Saisissez un nombre'
                 ],
             ])
             ->add('effet', TextType::class, [
-                'label' => "Effet(s)*",
+                'label' => "Effet(s)",
                 'attr' => [
                     'class' => 'form-control',
-                    'placeholder' => '-',
-                    'required' => true,
+                    'placeholder' => 'saignement',
                 ],
             ])
             ->add('poids', NumberType::class, [
-                'label' => "Poids* (0 si pas comptabilisé)",
+                'label' => "Poids*",
                 'attr' => [
-                    'class' => 'form-control',
+                    'class' => 'form-control required-field',
                     'placeholder' => 'Saisissez un nombre',
                     'required' => true,
                 ],
@@ -68,32 +86,14 @@ class ArmeType extends AbstractType
             ->add('prix', IntegerType::class, [
                 'label' => "Prix*",
                 'attr' => [
-                    'class' => 'form-control',
+                    'class' => 'form-control required-field',
                     'placeholder' => 'Saisissez un nombre',
                     'required' => true,
                 ],
             ])
-            ->add('taille', EntityType::class, [
-                'class' => Taille::class,
-                'choice_label' => 'libelle',
-                'label' => "Taille de l'arme*",
-                'required' => true,
-                'placeholder' => 'Choisir la taille', // Message par défaut
-                'attr' => [
-                    'class' => 'form-select',
-                ],
-            ])
 
-            ->add('typeArme', EntityType::class, [
-                'class' => TypeArme::class,
-                'choice_label' => 'libelle',
-                'label' => "Type d'arme*",
-                'required' => true,
-                'placeholder' => 'Choisir le type de l\'arme',
-                'attr' => [
-                    'class' => 'form-select',
-                ],
-            ])
+
+
         ;
     }
 
