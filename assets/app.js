e283919538ff4206cf11e542442bb32c705b33b3
Meya <angie-island@hotmail.fr>
2023-11-04 09:27:32 +0100

    correctif time msg flash

--- assets/app.js
+++ assets/app.js
@@ -4,10 +4,10 @@ const bootstrap = require('bootstrap');
 
 // #region message flash
 // Sélectionnez tous les éléments avec la classe "flash-message"
-const flashMessages = document.querySelectorAll('.alert-success');
+const flashMessages = document.querySelectorAll('.alert-time');
 
 // Définissez la durée pendant laquelle les messages flash resteront visibles (en millisecondes)
-const messageDuration = 2000; // 2 secondes
+const messageDuration = 1500; // 1.5 secondes
 
 // Masquez les messages flash après le délai défini
 flashMessages.forEach((message) => {
