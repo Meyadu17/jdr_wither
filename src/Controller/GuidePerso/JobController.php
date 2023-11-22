a836e8f5db73d9d9ee3daabd54b951fcdd436df5
Meya <angie-island@hotmail.fr>
2023-11-22 17:58:16 +0100

    editer job

--- src/Controller/GuidePerso/JobController.php
+++ src/Controller/GuidePerso/JobController.php
@@ -2,15 +2,74 @@
 
 namespace App\Controller\GuidePerso;
 
+use App\Entity\Job;
+use App\Form\JobType;
+use App\Repository\JobRepository;
+use App\Service\UploadService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+#[Route('/admin/job', name: 'app_admin_job')]
 class JobController extends AbstractController
 {
-    #[Route('/admin/job', name: 'app_job')]
-    public function index(): Response
+    const MESSAGE = "Le job";
+
+    #[Route('/ajouter', name: '_ajouter')]
+    #[Route('/modifier/{id}', name: '_modifier')]
+    public function index(Request $request,
+                          EntityManagerInterface $entityManager,
+                          JobRepository $jobRepository,
+                          UploadService $uploadService,
+                          int $id = null): Response
     {
-        return $this->render('job/listerJob.html.twig');
+        //Vérifier si c'est une création ou une modification
+        if($id == null){
+            $job = new Job();
+        }else{
+            $job = $jobRepository->find($id);
+        }
+
+        $form = $this->createForm(JobType::class, $job);
+        $form->handleRequest($request);
+
+        //Si le formulaire est valide
+        if($form->isSubmitted() && $form->isValid()) {
+
+            // Le formulaire est valide, vous pouvez accéder aux données
+            // Définir les bonus dans l'objet Job
+            $job->setBonusCaracteristiques($form->get('bonusCaracteristiques')->getData());
+            $job->setBonusTalent($form->get('bonusTalent')->getData());
+
+            // Upload de la photo
+            if ($form->get('photo')->getData()) {
+                $newFilename = $uploadService->upload($form->get('photo')->getData(),
+                                                      $this->getParameter('photo_directory'));
+                $job->setPhoto($newFilename);
+            }
+
+            //traitement des données
+            $entityManager->persist($job);
+            $entityManager->flush();
+
+            //message de succés
+            $this->addFlash(
+                'success',
+                self::MESSAGE . ' à bien été édité !'
+            );
+
+            if ($request->request->has('create_and_new')) {
+                // Redirige vers le formulaire avec un nouvel objet Arme
+                return $this->redirectToRoute('app_admin_job_ajouter');
+            } else {
+                return $this->redirectToRoute('app_guide_personnage_jobs');
+            }
+        }
+
+
+        return $this->render('guidePersonnage/job/editerJob.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
--- src/Form/JobType.php
+++ src/Form/JobType.php
@@ -3,19 +3,97 @@
 namespace App\Form;
 
 use App\Entity\Job;
+use App\Entity\Talent;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class JobType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('libelle')
-            ->add('presrequis')
-            ->add('bonusCaracteristiques')
-            ->add('bonusTalent')
+            ->add('libelle', TextType::class, [
+                'label' => "Nom*",
+                'attr' => [
+                    'class' => 'form-control required-field',
+                    'placeholder' => 'ex : elfe',
+                    'required' => true,
+                ],
+            ])
+            ->add('presrequis', TextType::class, [
+                'label' => "Pres requis",
+                'attr' => [
+                    'class' => 'form-control',
+                    'placeholder' => 'ex : elfe',
+                    'required' => true,
+                ],
+            ])
+            ->add('bonusCaracteristiques', ChoiceType::class, [
+                'label' => "Bonus aux caractéristiques",
+                'choices' => [
+                    'Force' => 'Force',
+                    'Charme' => 'Charme',
+                    'Sagacité' => 'Sagacité',
+                    'Habileté' => 'Habileté',
+                ],
+                'multiple' => true,
+                'expanded' => true, // Pour afficher les choix sous forme de checkbox
+                'attr' => [
+                    'class' => 'form-control form-check-input',
+                ],
+            ])
+            ->add('bonusTalent', ChoiceType::class, [
+                'label' => 'Bonus aux talents',
+                'choices' => [
+                    'Acrobaties' => 'Acrobaties',
+                    'Alchimie' => 'Alchimie',
+                    'Artisanat' => 'Artisanat',
+                    'Connaissances' => 'Connaissances',
+                    'Détection' => 'Détection',
+                    'Diplomatie' => 'Diplomatie',
+                    'Escalade' => 'Escalade',
+                    'Furtivité' => 'Furtivité',
+                    'Intuition' => 'Intuition',
+                    'Larcin' => 'Larcin',
+                    'Lutte' => 'Lutte',
+                    'Piège' => 'Piège',
+                    'Pistage' => 'Pistage',
+                    'Soins' => 'Soins',
+                    'Spectacle' => 'Spectacle',
+                    'Survie' => 'Survie',
+                ],
+                'multiple' => true,
+                'expanded' => true, // Pour afficher les choix sous forme de checkbox
+                'attr' => [
+                    'class' => 'form-control form-check-input',
+                ],
+            ])
+            ->add('photo', FileType::class, [
+                'label' => 'Photo (PNG, JPG, BMP)',
+                'mapped' => false,
+                'required' => false,
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/png',
+                            'image/jpeg',
+                            'image/bmp',
+                        ],
+                        'mimeTypesMessage' => 'Veuillez uploader un PNG ou un JPG',
+                    ])
+                ],
+            ])
         ;
     }
 
